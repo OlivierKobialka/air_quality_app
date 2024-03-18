7dbc7f64325a73405d62e324d78248cb94a8dbca
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-03-18 14:57:01 +0100

    Add dotenv and derive_more dependencies

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,9 @@
-use actix_web::{get, web, App, HttpServer};
+use actix_web::{
+    error, get,
+    http::{header::ContentType, StatusCode},
+    web, App, HttpResponse, HttpServer,
+};
+use derive_more::{Display, Error};
 use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
 
@@ -6,6 +11,7 @@ struct AppState {
     counter: i64,
 }
 
+// ========================== GET ==========================
 #[get("/")]
 async fn index() -> String {
     "Hello world!".to_string()
@@ -13,14 +19,11 @@ async fn index() -> String {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let app_data = web::Data::new(Mutex::new(AppState { counter: 0 }));
+    let app_data: web::Data<Mutex<AppState>> = web::Data::new(Mutex::new(AppState { counter: 0 }));
+    println!("[server] running at http://127.0.0.1:8080");
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(app_data.clone())
-            .service(index)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(move || App::new().app_data(app_data.clone()).service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
